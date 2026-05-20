087225d3992f922fbbc6950b3db666660731530c
Grace Pierce <gtp6910@gmail.com>
2026-05-20 17:13:36 -0400

    feat(tui): add print helper functions

--- lib/tui.sh
+++ lib/tui.sh
@@ -14,7 +14,6 @@ function hex_to_ansi() {
 }
 
 # sourcing / loading theme
-
 load_theme() {
   if [[ -f "$1" ]]; then
     source "$1"
@@ -22,6 +21,14 @@ load_theme() {
     printf "$(hex_to_ansi "#f87171")Error: theme file not found: $1${RESET}\n" # hard coded color bc the err color won't be loaded when it errs
   fi
 }
+
+# print helpers
+print_accent()  { printf "$(hex_to_ansi "$COLOR_ACCENT")$1${RESET}\n";}
+print_success() { printf "$(hex_to_ansi "$COLOR_SUCCESS")$1${RESET}\n";}
+print_warn()    { printf "$(hex_to_ansi "$COLOR_WARN")$1${RESET}\n";}
+print_error()   { printf "$(hex_to_ansi "$COLOR_ERROR")$1${RESET}\n";}
+print_muted()   { printf "$(hex_to_ansi "$COLOR_MUTED")$1${RESET}\n";}
+print_text()    { printf "$(hex_to_ansi "$COLOR_TEXT")$1${RESET}\n";}
 
 # testing
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
@@ -31,4 +38,12 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   # test 2: load_theme function
   load_theme "config/themes/mono.conf"
   printf "$(hex_to_ansi "$COLOR_ACCENT")accent color loaded\n${RESET}"
+
+  # test 3: print helpers
+  print_accent "accent test"
+  print_error "err test"
+  print_muted "muted test"
+  print_success "success test"
+  print_text "text test"
+  print_warn "warn test"
 fi
