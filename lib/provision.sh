43976123e656ded3f0e3b6ac51ca4bbbd6ced565
Grace Pierce <gtp6910@gmail.com>
2026-05-22 11:56:24 -0400

    fix(provision): fix next_ct_id for when no CT at run

--- lib/provision.sh
+++ lib/provision.sh
@@ -4,6 +4,13 @@ source "$(dirname "$0")/../config/defaults.conf"
 
 # outputs next CT number
 function next_ct_id() {
+    print_text "Fetching CT ID"
     local HIGHEST=${ssh root@$PVE_HOST "pct list | awk 'NR>1 {print \$1}' | sort -n | tail -1"} # cmd by Sonnet 4.6
-    echo $(( HIGHEST++ ))
-}   
+    if ! [[ "$HIGHEST" =~ ^[+-]?[0-9]+$ ]]; then
+        print_warn "Could not find any CT IDs, defaulting to 100"
+        HIGHEST=100
+    fi
+    local NEW_ID = (( HIGHEST + 1 ))
+    print_success "New CT ID: $NEW_ID"
+    echo $NEW_ID
+}
