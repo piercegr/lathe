f44017469d65dba277a7312f14c2df719e1c1634
Grace Pierce <gtp6910@gmail.com>
2026-05-22 14:21:21 -0400

    chore(main): setup main sourcing

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# globals
+source "$(dirname "$0")/lib/tui.sh"
+source "$(dirname "$0")/lib/secrets.sh"
+source "$(dirname "$0")/lib/provision.sh"
+source "$(dirname "$0")/lib/docker.sh"ss
