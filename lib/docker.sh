3547298297e1f1076629101f72a5cd3a90c1989a
Grace Pierce <gtp6910@gmail.com>
2026-05-22 13:32:48 -0400

    feat(docker): add select_tag

--- lib/docker.sh
+++ lib/docker.sh
@@ -21,3 +21,22 @@ function search_image() { # TODO: allow the user to go back and select another i
   local IMAGE=$(select_option "Select an image:" "${options[@]}")
   echo "$IMAGE"
 }
+
+# get available tags for an image
+function select_tag() {
+  local IMAGE="$1"
+  local RESULTS=$(curl -s "https://hub.docker.com/v2/repositories/$IMAGE/tags/?page_size=10" | jq -r '.results[].name')
+
+  if [[ -z "$RESULTS" ]]; then
+    print_error "error: could not fetch tags for '$IMAGE'"
+    exit 1
+  fi
+
+  local options=()
+  while IFS= read -r line; do
+    options+=("$line")
+  done <<< "$RESULTS"
+
+  local TAG=$(select_option "Select a tag:" "${options[@]}")
+  echo "$TAG"
+}
