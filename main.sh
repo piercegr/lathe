5bbfc9716e4f642f77789130c90b481f3b716d1f
Grace Pierce <gtp6910@gmail.com>
2026-05-22 14:46:33 -0400

    chore: init-semver

--- main.sh
+++ main.sh
@@ -5,3 +5,26 @@ source "$(dirname "$0")/lib/secrets.sh"
 source "$(dirname "$0")/lib/provision.sh"
 source "$(dirname "$0")/lib/docker.sh"ss
 
+check_single_dep() {
+if command -v $1 &>/dev/null; then
+    return 0
+  else
+    print_error "error: the cmd '$1' does not exist"
+    exit 1
+  fi
+}
+
+check_deps() {
+  check_single_dep "gpg"
+  check_single_dep "ssh"
+  check_single_dep "curl"
+  check_single_dep "jq"
+  check_single_dep "git"
+}
+
+main() {
+  check_deps
+  load_theme
+  load_org_secrets
+  load_personal_secrets
+}
